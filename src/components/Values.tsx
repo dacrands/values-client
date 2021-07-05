9b255c04b24b975c90fc9d1d831b20baa2598fed
David Crandall <davecrands@gmail.com>
2021-07-05 17:47:07 -0500

    Add Values form

--- src/components/Values.tsx
+++ src/components/Values.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 interface Value {
-    _id: string;
+    _id?: string;
     name: string;
     description: string;
     importance: number;
@@ -9,20 +10,61 @@ interface Value {
 
 export default function Values() {
     const [values, setValues] = useState<Value[]>([]);
-    const fetchValues = () => {
-        fetch("http://localhost:7000/api/values")
+    const [value, setValue] = useState<Value>({
+        name: "",
+        description: "",
+        importance: 0
+    });
+    const fetchValues = async () => {
+        await fetch("http://localhost:7000/api/values")
         .then(response => response.json())
         .then(data => setValues([...data]));
+    }
+    const addValue = async (value: Value) => {
+        await fetch("http://localhost:7000/api/values", {
+            method: "POST",            
+            body: JSON.stringify(value),
+            headers: {
+                'Content-Type': 'application/json'              
+            },
+        });       
+    }
+    const handleSubmit = async (e: React.SyntheticEvent): Promise<void> => {
+        e.preventDefault();
+        await addValue(value);
+        await fetchValues();
+        setValue({
+            name: "",
+            description: "",
+            importance: 0
+        });
+    }
+    const onChange = (e: React.FormEvent<HTMLInputElement>): void => {
+        const { name, value } = e.target as HTMLTextAreaElement;
+        setValue(prevState  => ({
+            ...prevState,
+            [name]: value
+        }));
     }
     useEffect(() => {
         fetchValues();
     }, [])
+
     return <>
         <h2>Your Values</h2>
-        <ul>
+        <form className="form" onSubmit={handleSubmit}>
+            <input className="form-item" type="text" placeholder="Name something you value..." name="name" value={value.name} onChange={onChange} />
+            <input className="form-item" type="text" placeholder="Describe the value..." name="description" value={value.description} onChange={onChange} />
+            <input className="form-item" type="number" name="importance" value={value.importance} onChange={onChange} />
+            <span>On a 1-10 scale, how important is the value?</span>
+            <button className="form-item" type="submit">Submit</button>
+        </form>
+        <ul className="plain-list card-grid">
         {
             values.map((value) => {
-                return <li key={value._id}>{value.name}</li>
+                return <li className="card" key={value._id}>
+                    <Link to={`value/${value._id}`}>{value.name}</Link>
+                </li>
             })
         }
         </ul>
