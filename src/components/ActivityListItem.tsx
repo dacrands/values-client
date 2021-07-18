4a375e2912e93f1bae516403e428b564555af7f5
David Crandall <davecrands@gmail.com>
2021-07-17 19:29:42 -0500

    Add Link to ActivityListItem

--- src/components/ActivityListItem.tsx
+++ src/components/ActivityListItem.tsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import IActivityListItemProps from "../interfaces/IActivityListItemProps";
 
 
 export default function ActivityListItem(props: IActivityListItemProps) {
     return (
-        <li>
-            <h4>{props.activity.name}</h4>  
-            <p>{props.activity.duration} minutes</p>
-            {props.activity.createdAt &&
-                <p>{new Date(props.activity.createdAt).toLocaleString()}</p>
-            }                                                  
+        <li className="card">
+            <Link to={`/activities/${props.activity._id}/${props.activity.name}`}>
+                <h4>{props.activity.name}</h4>  
+                <p>{props.activity.duration} minutes</p>
+                {props.activity.createdAt &&
+                    <p>{new Date(props.activity.createdAt).toLocaleString()}</p>
+                }     
+            </Link>                                             
         </li>
     )
 }
