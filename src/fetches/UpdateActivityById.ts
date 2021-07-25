ee7d520b8377eb5def2206a076a67fea5682b6d9
David Crandall <davecrands@gmail.com>
2021-07-25 15:39:02 -0500

    Add updateActivityById

---
+++ src/fetches/UpdateActivityById.ts
@@ -0,0 +1,13 @@
+import IActivity from "../interfaces/IActivity";
+
+const updateActivityById = async (id: string, activity: IActivity) => {
+    await fetch(`http://localhost:7000/api/activities/${id}`, {
+        method: "PUT",            
+        body: JSON.stringify(activity),
+        headers: {
+            'Content-Type': 'application/json'              
+        },
+    });       
+}
+
+export default updateActivityById;
