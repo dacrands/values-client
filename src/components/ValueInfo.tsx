07d8d94ad953f8ba79f316f559fed1ac86df8c9c
David Crandall <davecrands@gmail.com>
2021-08-01 16:01:29 -0500

    Add edit value logic

--- src/components/ValueInfo.tsx
+++ src/components/ValueInfo.tsx
@@ -6,10 +6,12 @@ import IActivity from "../interfaces/IActivity";
 import ActivityListItem from "./ActivityListItem";
 import fetchValueById from "../fetches/FetchValueById";
 import deleteValueById from "../fetches/DeleteValueById";
+import updateValueById from "../fetches/UpdateValueById";
 
 export default function ValueInfo() {
   const history = useHistory();
   const { id, name } = useParams<{ id: string; name: string }>();
+  const [isEditing, setIsEditing] = useState<boolean>(false);
   const [activities, setActivities] = useState<IActivity[]>([]);
   const [value, setValue] = useState<IValue>({
     name: "",
@@ -17,18 +19,69 @@ export default function ValueInfo() {
     importance: 0,
   });
 
+  const onChange = (e: React.FormEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target as HTMLTextAreaElement;
+    setValue((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.SyntheticEvent): Promise<void> => {
+    e.preventDefault();
+    await updateValueById(id, value);
+    await fetchValueById(id, setValue, setActivities);
+    setIsEditing(false);
+  };
+
   useEffect(() => {
     fetchValueById(id, setValue, setActivities);
   }, []);
 
   return (
     <div className="container">
-      <header className="header">
-        <h2>{name}</h2>
-        <p>Description: {value.description}</p>
-        <p>Importance: {value.importance}</p>
-      </header>
-
+      <div className="flex-center">
+        {isEditing ?
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="form-item"
+              name="name"
+              value={value.name}
+              onChange={onChange}
+            />
+            <input
+              type="text"
+              className="form-item"
+              name="description"
+              value={value.description}
+              onChange={onChange}
+            />
+            <input
+              type="number"
+              className="form-item"
+              name="importance"
+              value={value.importance}
+              onChange={onChange}
+            />
+            <div className="text-center">
+              <button className="btn btn-primary" type="submit">
+                Submit
+              </button>
+            </div>
+          </form>
+          : <header className="header">
+            <h2>{value.name === "" ? name : value.name}</h2>
+            <p>Description: {value.description}</p>
+            <p>Importance: {value.importance}</p>
+          </header>
+        }
+        <div style={{marginTop: "1rem" }}>
+          <button className="btn" onClick={() => setIsEditing(!isEditing)}>
+            Edit
+          </button>
+        </div>
+      </div>
       <h3 style={{ textAlign: "center" }}>Activities</h3>
       <ul className="plain-list card-grid">
         {activities.map((activity) => {
